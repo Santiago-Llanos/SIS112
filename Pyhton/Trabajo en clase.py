8d904a422b6223f87d7ed8e132e925178ea71228
Santiago-Llanos <santiago.llanos@ucb.edu.bo>
2024-08-16 10:36:17 -0400

    python
    Ejercicio python

---
+++ Pyhton/Trabajo en clase.py
@@ -0,0 +1,22 @@
+def Hola_Mundo():
+   print("Hola mundo")
+
+
+
+def Obtener_Edad():
+   edad = input("Ingrese su edad: ")
+   print ("tu edad es: ", edad)
+
+   
+
+
+def Calcular_Edad():
+   anioNacimiento = int(input("Ingresa tu año de nacimiento:"))
+   edad = 2024 - anioNacimiento
+   print ("su edad calculada es: ", edad)
+
+Hola_Mundo()
+Obtener_Edad()
+Calcular_Edad()
+
+
