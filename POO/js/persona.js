37e6bf3f07126856dd719ea282ce37a8713e9754
Santiago-Llanos <santiago.llanos@ucb.edu.bo>
2024-08-26 12:26:38 -0400

    Tarea

--- POO/js/persona.js
+++ POO/js/persona.js
@@ -1,8 +1,10 @@
 class Persona {
-    constructor(nombre, edad, carrera) {
+   constructor(nombre, edad, carrera, universidad) {
       this.nombre = nombre;
       this.edad = edad;
       this.carrera = carrera;
+      this.universidad = universidad;
+      
     }
   
     saludar() {
@@ -16,18 +18,28 @@ class Persona {
     estudiar() {
       return 'Estoy estudiando ' + this.carrera;
     }
-  }
-  
+
+    Universidad() {
+        return 'Estudio en la universidad ' + this.universidad;
+    }
+
+    Modificar_Saludo() {
+        this.nombre = prompt ("Ingrese su nuevo nombre")
+        return 'Buenasss mi nombre es' + this.nombre;
+   }
+}
   // Crear una instancia de la clase Persona
-  const persona = new Persona('Juan', 25, 'Ingeniería');
+  const persona = new Persona('Santiago', 18, 'Ingeniería Industrial', 'Catolica', 'Saludo');
   
   // Inicializar los elementos HTML
-  const saludar = document.getElementById('saludar');
+  const Saludar = document.getElementById('saludar'); //obtener cosas de hatml a js, document.getElementById (id, escrito igual)
   const edad = document.getElementById('edad');
   const carrera = document.getElementById('carrera');
-  
+  const universidad = document.getElementById('universidad');
+  const saludo = document.getElementById('saludo');
+
   // Función para saludar
-  function botonSaludar() {
+   function botonSaludar() {
     saludar.textContent = persona.saludar();
   }
   
@@ -41,5 +53,12 @@ class Persona {
     carrera.textContent = persona.estudiar();
   }
 
+  function botonUniversidad() {
+    universidad.textContent = persona.Universidad();
+  }
 
+  function botonModificarSaludo() {
+    saludo.textContent = persona.Modificar_Saludo();
+    
+  }
 
