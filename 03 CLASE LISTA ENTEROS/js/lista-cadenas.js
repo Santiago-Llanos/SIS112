87e57b82e8ceb2e77f506e424f3dbac09a14f525
Santiago-Llanos <santiago.llanos@ucb.edu.bo>
2024-10-07 12:22:11 -0400

    lista cadenas

--- 03 CLASE LISTA ENTEROS/js/lista-cadenas.js
+++ 03 CLASE LISTA ENTEROS/js/lista-cadenas.js
@@ -23,7 +23,73 @@ class ListaCadenas {
     }
 }
 
-let miLista = new ListaCadenas();
+const miLista = new ListaCadenas();
+miLista.agregar('Hola'); //4 -0
+miLista.agregar('Mundo');// 5 - 1
+miLista.agregar('UCB');// 3 - 2
+
+
+// Retornar la cadena con mayor caracteres
+
+// var = let
+// var a = miLista.lista[0]
+// console.log(a);
+// var aCant = a.length;
+// console.log(aCant);
+
+// var b = miLista.lista[1]
+// console.log(b);
+// var bCant = b.length
+// console.log(bCant);
+
+// var c = miLista.lista[2]
+// console.log(c);
+// var cCant = c.length
+// console.log(cCant);
+
+//Buscar el mayor
+// var mayor = 0;
+// var posicion = -1;
+// if (aCant > mayor) {
+//     mayor = aCant
+//     posicion = 0;
+// }
+// //---
+// if (bCant > mayor){
+//     mayor = bCant;
+//     posicion = 1;
+// }
+// //---
+// if (cCant > mayor){
+//     mayor = cCant;
+//     posicion = 2;
+// }
+// console.log('La mayor cantidad de caracteres tiene: ' + mayor);
+// console.log('La CADENA con mayor cantidad de caracteres es: ' + miLista.lista[posicion]);
+
+// Realizar el mismo ejercicio pero con un for 
+var mayor = 0;
+var posicion = -1;
+
+for (var i = 0; i < miLista.lista.length; i++) {
+    var cadena = miLista.lista[i];
+    var cantidad = cadena.length;
+
+    console.log(cadena);
+    console.log(cantidad);
+
+    if (cantidad > mayor) {
+        mayor = cantidad;
+        posicion = i;
+    }
+}
+
+console.log('La mayor cantidad de caracteres tiene: ' + mayor);
+console.log('La CADENA con mayor cantidad de caracteres es: ' + miLista.lista[posicion]);
+
+
+
+console.log(miLista.lista);
 
 function actualizarResultado() {
     document.getElementById('result').innerHTML = `Resultado: [ ${miLista.lista.join(', ')} ]`;
